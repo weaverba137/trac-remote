aff919ba93b9ca463060e3ef90085e08e2b68430
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2013-10-06 14:54:05 -0400

    Add option parser to main

--- TracRemote/main.py
+++ TracRemote/main.py
@@ -1,4 +1,16 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+#
 def main():
+    """Main entry point for the trac-remote script."""
+    description = """Foo Bar"""
+    try:
+        import argparse
+        parser = argparse.ArgumentParser(description=description)
+    except ImportError:
+        import optparse
+        parser = optparse.OptionParser(usage=description)
+    parser.parse_args()
+    print("Foo Bar")
     return
